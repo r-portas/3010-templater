58a12e38b3549da546c5050a1cb2cfda4a7355ea
Roy Portas <royportas@gmail.com>
2016-03-05 11:59:40 +1000

    Fixed setup script

--- setup.js
+++ setup.js
@@ -1,4 +1,11 @@
-var fs = require('fs');
+var fs = require('fs.extra');
 
 var cmd = "#!/bin/bash\nnode " + __dirname + "/main.js";
 fs.writeFileSync('mylib-gen', cmd, 'utf-8');
+fs.copy(__dirname + '/mylib-gen', '/usr/bin/mylib-gen', {replace: true}, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Added to path.");
+  }
+});
