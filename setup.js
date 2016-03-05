4676e432c80878a353cb64261306315958178210
Roy Portas <royportas@gmail.com>
2016-03-05 12:31:17 +1000

    Fixed file copying issues

--- setup.js
+++ setup.js
@@ -1,12 +1,4 @@
-var fs = require('fs.extra');
-
 var cmd = "#!/bin/bash\nnode " + __dirname + "/main.js";
 fs.writeFileSync('mylib-gen', cmd, 'utf-8');
-fs.chmodSync('mylib-gen', '777');
-fs.copy(__dirname + '/mylib-gen', '/usr/bin/mylib-gen', {replace: true}, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Added to path.");
-  }
-});
+fs.createReadStream('./mylib-gen').pipe(fs.createWriteStream('/usr/sbin/mylib-gen'));
+fs.chmodSync('/usr/sbin/mylib-gen', '777');
